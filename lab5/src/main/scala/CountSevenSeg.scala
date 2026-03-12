d3609da5d54403794347f89e0ceb099db6703d44
norbjerg <lassenorbjerg@hotmail.com>
2026-03-12 14:34:42 +0100

    week6

--- lab5/src/main/scala/CountSevenSeg.scala
+++ lab5/src/main/scala/CountSevenSeg.scala
@@ -7,9 +7,23 @@ class CountSevenSeg extends Module {
   })
 
   val sevSeg = WireDefault("b1111111".U(7.W))
-
   // *** your code starts here
+  val sevSegDec = Module(new SevenSegDecoder())
+  val clk_reg = RegInit(0.U(4.W))
+  val cnt_reg = RegInit(0.U(4.W))
 
+  val MAX_CLK = 50000000
+  val MAX_CNT = 16
+
+  clk_reg := clk_reg + 1.U
+  when (clk_reg >= (MAX_CLK - 1).U) {
+    clk_reg := 0.U
+    cnt_reg := cnt_reg + 1.U
+    when (cnt_reg >= (MAX_CNT - 1).U) {
+      cnt_reg := 0.U
+    }
+  }
+  sevSegDec.io.sw := cnt_reg
 
   // *** your code ends here
 
--- lab5/src/main/scala/SevenSegDecoder.scala
+++ lab5/src/main/scala/SevenSegDecoder.scala
@@ -11,7 +11,24 @@ class SevenSegDecoder extends Module {
   val sevSeg = WireDefault(0.U(7.W))
 
   // ***** your code starts here *****
-
+  switch (io.sw) {
+    is("h0".U){sevSeg:=(0x4+0x8+0x10+0x20+0x01+0x02).U}
+    is("h1".U){sevSeg:=(0x4+0x2).U}
+    is("h2".U){sevSeg:=(0x1+0x2+0x40+0x10+0x8).U}
+    is("h3".U){sevSeg:=(0x1+0x2+0x40+0x4+0x8).U}
+    is("h4".U){sevSeg:=(0x20+0x2+0x40+0x4).U}
+    is("h5".U){sevSeg:=(0x1+0x20+0x40+0x4+0x8).U}
+    is("h6".U){sevSeg:=(0x1+0x20+0x40+0x10+0x8+0x4).U}
+    is("h7".U){sevSeg:=(0x4+0x2+0x1).U}
+    is("h8".U){sevSeg:=("b1111111").U}
+    is("h9".U){sevSeg:=(0x01+0x20+0x2+0x40+0x8+0x4).U}
+    is("ha".U){sevSeg:=("b1110111").U}
+    is("hb".U){sevSeg:=("b1111100").U}
+    is("hc".U){sevSeg:=("b0111001").U}
+    is("hd".U){sevSeg:=("b1011110").U}
+    is("he".U){sevSeg:=("b1111001").U}
+    is("hf".U){sevSeg:=("b1110001").U}
+  }
   // ***** your code ends here *****
 
   io.seg := ~sevSeg
